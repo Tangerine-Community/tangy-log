0b0fab1bf9e1882ee6d91cc97099dbbedb5f9ac6
R.J. Steinert <rj@rjsteinert.com>
2018-05-31 13:09:45 -0400

    Add module

---
+++ index.js
@@ -0,0 +1,57 @@
+const chalk = require('chalk')
+const LOG_LEVEL_INFO = 'INFO'
+const LOG_LEVEL_WARNING = 'WARNING'
+const LOG_LEVEL_ERROR = 'ERROR'
+const LOG_LEVEL_DEBUG = 'DEBUG'
+
+const log = function(level = LOG_LEVEL_INFO, message = '') {
+  const dateTime = new Date().toISOString()
+  // Line details example from https://stackoverflow.com/a/39684350
+  let logLineDetails = ((new Error().stack).split("at ")[3]).trim();
+  // Allow shorthand log(message) for debugging.
+  if (message === '') {
+    message = level
+    level = LOG_LEVEL_DEBUG
+  }
+  // Stringify objects.
+  if (typeof message === 'object' && level !== LOG_LEVEL_DEBUG) {
+    message = `${JSON.stringify(message)}`
+  } else if (typeof message === 'object' && level === LOG_LEVEL_DEBUG) {
+    // Debug object messages go on new lines and pretty printed.
+    message = `\n${JSON.stringify(message, null, 2)}`
+  }
+  const logEntry = `${dateTime} ${level} ${logLineDetails} ${message}`
+  switch (level) {
+    case LOG_LEVEL_INFO:
+      console.log(chalk.green(logEntry))
+      break
+    case LOG_LEVEL_WARNING:
+      console.log(chalk.yellow(logEntry))
+      break
+    case LOG_LEVEL_ERROR:
+      console.log(chalk.red(logEntry))
+      break
+    case LOG_LEVEL_DEBUG:
+      console.log(chalk.magenta(logEntry))
+      break
+  }
+}
+
+exports.clog = function(message) {
+  log(LOG_LEVEL_DEBUG, message)
+}
+
+exports.TangerineLog = {
+  info: function(message) {
+    log(LOG_LEVEL_INFO, message)
+  },
+  warn: function(message) {
+    log(LOG_LEVEL_WARNING, message)
+  },
+  error: function(message) {
+    log(LOG_LEVEL_ERROR, message)
+  },
+  debug: function(message) {
+    log(LOG_LEVEL_DEBUG, message)
+  }
+}
---
+++ test.js
@@ -0,0 +1,15 @@
+const log = require('./index.js').TangerineLog
+// clog function is a shortcut to output messages with LOG_LEVEL_DEBUG. It's also an easy thing to grep for since you 
+// shouldn't have debug messages in your codebase.
+const clog = require('./index.js').clog
+
+const test = function() {
+  log.info('Hello info')
+  log.warn({ message: 'Hello warning'} )
+  log.error('Does not compute.')
+  log.debug('Deeeeeebug')
+  log.debug({ interesting: 'giraffe'})
+  clog({ happy: 'tiger'})
+}
+
+test()
